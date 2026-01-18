5850d381e838a85f8cf9ebc40a9ebda1cdf8e85c
eziochiu <eziochiuchina@hotmail.com>
2026-01-18 19:18:46 +0800

    fix flycast speed to fast

--- libretrodroid/src/main/cpp/environment.cpp
+++ libretrodroid/src/main/cpp/environment.cpp
@@ -327,8 +327,6 @@ bool Environment::handle_callback_environment(unsigned cmd, void *data) {
             *((unsigned*) data) = language;
             return true;
 
-        // 解决 Flycast 等核心运行过快的问题
-        // 当核心查询是否处于快进模式时，返回 false 表示正常速度运行
         case RETRO_ENVIRONMENT_GET_FASTFORWARDING:
             LOGD("Called RETRO_ENVIRONMENT_GET_FASTFORWARDING");
             *((bool*) data) = false;
