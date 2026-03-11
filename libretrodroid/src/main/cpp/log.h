9fcf2ac56851c14c4305b30e0de96eadbfb5a16f
eziochiu <eziochiuchina@hotmail.com>
2026-03-11 15:41:32 +0800

    close log

--- libretrodroid/src/main/cpp/log.h
+++ libretrodroid/src/main/cpp/log.h
@@ -20,7 +20,7 @@
 
 #define MODULE_NAME "libretrodroid"
 
-#define VERBOSE_LOGGING true
+#define VERBOSE_LOGGING false
 #define GLES_LOGGING false
 
 #if GLES_LOGGING
@@ -43,8 +43,8 @@
 
 #define LOGV(...)
 #define LOGD(...)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, MODULE_NAME, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, MODULE_NAME, __VA_ARGS__)
+#define LOGI(...)
+#define LOGW(...)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, MODULE_NAME, __VA_ARGS__)
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, MODULE_NAME, __VA_ARGS__)
 
